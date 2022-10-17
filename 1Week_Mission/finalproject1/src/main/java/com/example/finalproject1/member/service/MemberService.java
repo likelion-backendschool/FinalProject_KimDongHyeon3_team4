448a994929adf1a3a6cdbb54a7038c679e13e452
kimdonghyeon3 <sh2115521@naver.com>
2022-10-18 01:33:55 +0900

    chore #9 : 회원가입 후 이메일 발송

--- 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/service/MemberService.java
+++ 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/service/MemberService.java
@@ -3,6 +3,8 @@ package com.example.finalproject1.member.service;
 import com.example.finalproject1.member.entity.Member;
 import com.example.finalproject1.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender javaMailSender;
     public void save(String username, String password, String email, String nickname) {
 
         Member member = Member.builder()
@@ -22,5 +25,11 @@ public class MemberService {
                 .build();
 
         memberRepository.save(member);
+
+        SimpleMailMessage emailMessage = new SimpleMailMessage();
+        emailMessage.setTo(email);
+        emailMessage.setText("가입을 축하합니다.");
+        emailMessage.setSubject("서비스 가입을 완료하였습니다.");
+        javaMailSender.send(emailMessage);
     }
 }
