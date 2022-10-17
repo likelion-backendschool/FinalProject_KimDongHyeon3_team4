3fd925a4d672a6b796bd7a0edd00c6f93182c06c
kimdonghyeon3 <sh2115521@naver.com>
2022-10-17 20:04:41 +0900

    chore #7 : layout + toastr 수정

--- 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/controller/MemberController.java
+++ 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/controller/MemberController.java
@@ -1,13 +1,30 @@
 package com.example.finalproject1.member.controller;
 
+import com.example.finalproject1.member.dto.JoinForm;
+import com.example.finalproject1.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/join")
     public String joinShow(){
+        return "/member/join";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid JoinForm joinForm){
         return "/member/join";
     }
 }
