5a10b1b307a7a14dc4d49fe193df8f3419fceec2
kimdonghyeon3 <sh2115521@naver.com>
2022-10-17 18:08:05 +0900

    feat #1 : init setting

---
+++ 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/Finalproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.finalproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Finalproject1Application.class, args);
+    }
+
+}
---
+++ 1Week_Mission/finalproject1/src/test/java/com/example/finalproject1/Finalproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
