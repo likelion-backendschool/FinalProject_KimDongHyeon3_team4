72bee6c704706be718604f905ae65567b0c3df74
kimdonghyeon3 <sh2115521@naver.com>
2022-10-17 19:40:20 +0900

    feat #6 : layout template 적용

--- 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/controller/MemberController.java
+++ 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/controller/MemberController.java
@@ -1,7 +1,13 @@
 package com.example.finalproject1.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
+
+    @GetMapping("/join")
+    public String joinShow(){
+        return "/member/join";
+    }
 }
--- 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/entity/Member.java
+++ 1Week_Mission/finalproject1/src/main/java/com/example/finalproject1/member/entity/Member.java
@@ -1,11 +1,33 @@
 package com.example.finalproject1.member.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column
+    String username;
+
+    @Column
+    String password;
+
+    @Column
+    String email;
+
+    @Column
+    String nickname;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
 }
