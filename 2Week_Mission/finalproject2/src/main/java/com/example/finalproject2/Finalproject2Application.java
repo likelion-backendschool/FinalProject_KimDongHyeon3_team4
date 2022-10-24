eedf7221fb98a402463c8730d36882a84a0d4421
kimdonghyeon3 <sh2115521@naver.com>
2022-10-24 15:48:32 +0900

    chore #36 : 2Week_Mission init and 1Week_Mission 가져오기 수정

---
+++ 2Week_Mission/finalproject2/src/main/java/com/example/finalproject2/Finalproject2Application.java
@@ -0,0 +1,13 @@
+package com.example.finalproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalproject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Finalproject2Application.class, args);
+    }
+
+}
---
+++ 2Week_Mission/finalproject2/src/test/java/com/example/finalproject2/Finalproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalproject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
